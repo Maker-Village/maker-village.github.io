4d8ce3c5656653cb9ac8e5f92d95afffc12ce6fb
david11014 <david11014@gmail.com>
2015-10-26 09:58:12 +0800

    結構整理 footer修改

---
+++ js/load.js
@@ -0,0 +1,42 @@
+//init
+$(document).ready(function(){
+  $('.scrollspy').scrollSpy();
+  $(".button-collapse").sideNav({
+    edge: 'right',
+    closeOnClick: true
+  });
+   Loaddata();
+});
+
+//scroll
+$( window ).scroll(function() {
+  //logo & navbar
+  if($(document).scrollTop()>250){
+    $('nav .brand-logo .top').removeClass("top");
+    $('nav .brand-logo img').attr("src","./pic/LOGO-2.png");
+
+    $('nav .nav-wrapper').addClass("alpha-5");
+  }else{
+    $('nav .brand-logo img').addClass("top");
+    $('nav .brand-logo img').attr("src","./pic/LOGO.png");
+    
+    $('nav .nav-wrapper').removeClass("alpha-5");
+  }
+});
+
+function Loaddata(){
+
+  console.log(data );
+  /*
+  var StyleHead = '<li style="white-space: nowrap;"><a  style="display: block;" onclick="ChangeYear(' // href="javascript:ChangeYear(';
+  var StyleMiddle1 = ')" touchstart="ChangeYear(';
+  var StyleMiddle2 = ')"> ';
+  var StyleFoot = '</a></li>';
+  $(".y").empty(); //清空menu內容
+  var str = "";
+  for(var i =0; i < data.length; i++)
+  {
+    str = str + StyleHead + i + StyleMiddle1 + i + StyleMiddle2 + title_str(i) + StyleFoot;
+  }
+  $(".y").append(str);*/
+}
